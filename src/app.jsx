e7936a6bd8d7b6793098f20557ad513d355aa104
Ali Daley <ali.daley72@gmail.com>
2021-11-30 00:18:39 -0500

    Created two api calls for getting the songs by ID and getting all the songs

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,46 @@
 import React, {Component} from "react";
+import axios from 'axios';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            songs: []
         }
     }
 
     componentDidMount(){
+        this.getSongs();
     }
 
-    render() { 
+    async getSongs(){
+        try{
+            let response = await axios.get('http://www.devcodecampmusiclibrary.com/api/music/');
+            this.setState({
+                songs:response.data
+            });
+        }
+        catch (error) {
+            console.log('Error in API call:', error);
+        }
+    }
+
+    async getSongById(songID){
+        try{
+            let response = axios.get(`http://www.devcodecampmusiclibrary.com/api/music/${songID}`);
+            this.setState({
+                songs:response.data
+            });
+        }
+        catch (error) {
+            console.log('Error in API call:', error);
+        }
+    }
+
+    render(){ 
         return ( 
             <React.Fragment>
-                
+
             </React.Fragment>
         );
     }
